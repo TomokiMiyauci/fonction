5f41a49aeddcf083c835427a6ebfc6e26c2bc37a
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-04-16 21:36:27 +0900

    :sparkles:  Add sum function

---
+++ src/sum.ts
@@ -0,0 +1,16 @@
+import { add } from '@/add'
+import { InferArray } from '@/types'
+
+/**
+ * Adds together all the elements of a list.
+ *
+ * @param val - list An array of numbers
+ * @returns The sum of all the numbers in the list
+ *
+ * @example
+ * sum([1, 2, 3, 4, 5]) // 15
+ */
+const sum = <T extends number[]>(val: T): InferArray<T> =>
+  val.reduce((acc, cur) => add(acc, cur), 0) as InferArray<T>
+
+export { sum }
