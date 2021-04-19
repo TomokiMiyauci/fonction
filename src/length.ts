01a49057d70ac2cb608f348e27f865ac904a47ac
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-04-19 17:46:27 +0900

    :sparkles:  Add length function

---
+++ src/length.ts
@@ -0,0 +1,14 @@
+/**
+ * Returns the number of elements in the array or string length.
+ *
+ * @param val - `string` or any `array`
+ * @returns The result of `val.length`
+ *
+ * @example
+ *
+ *
+ * @beta
+ */
+const length = <T extends unknown[] | string>(val: T): T['length'] => val.length
+
+export { length }
