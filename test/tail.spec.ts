33794169522be68cbcb8761af7c1999cb88c2ed6
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-04-19 23:34:10 +0900

    :rotating_light:  Fix lint error

--- test/tail.spec.ts
+++ test/tail.spec.ts
@@ -1,11 +1,18 @@
 import { tail } from '@/tail'
 
 describe('tail', () => {
-  const table: [string | unknown[], string | unknown[]][] = [
+  const tableString: [string, string][] = [
     ['', ''],
     ['a', ''],
     ['ab', 'b'],
-    ['abc', 'bc'],
+    ['abc', 'bc']
+  ]
+
+  it.each(tableString)('tail(%s) -> %s', (val, expected) => {
+    expect(tail(val)).toEqual(expected)
+  })
+
+  const tableArray: [unknown[], unknown[]][] = [
     [[], []],
     [[''], []],
     [[undefined], []],
@@ -26,7 +33,7 @@ describe('tail', () => {
     [[['hello', 'world']], []]
   ]
 
-  it.each(table)('tail(%s) -> %s', (val, expected) => {
+  it.each(tableArray)('tail(%s) -> %s', (val, expected) => {
     expect(tail(val)).toEqual(expected)
   })
 })
