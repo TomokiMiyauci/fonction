617a3c4c9f217e3a0b51c39b100600fc52695748
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-05-10 14:55:28 +0900

    :zap:  Remove unused output

--- scripts/post-build.ts
+++ scripts/post-build.ts
@@ -1,5 +1,7 @@
 import { removeSync } from 'fs-extra'
+import { resolve } from 'path'
 
-removeSync('dist/index.umd.d.ts')
-removeSync('dist/index.umd.d.ts.map')
+const dist = resolve(__dirname, '..', 'dist')
+removeSync(resolve(dist, 'index.umd.d.ts'))
+removeSync(resolve(dist, 'index.umd.d.ts.map'))
 // removeSync('temp')
