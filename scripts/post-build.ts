c9decd627a54bc3926bc8866186badafa8c75d1e
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-05-10 22:05:28 +0900

    :bug:  Fix to resolve internal module on bundling

--- scripts/post-build.ts
+++
@@ -1,7 +0,0 @@
-import { removeSync } from 'fs-extra'
-import { resolve } from 'path'
-
-const dist = resolve(__dirname, '..', 'dist')
-removeSync(resolve(dist, 'index.umd.d.ts'))
-removeSync(resolve(dist, 'index.umd.d.ts.map'))
-// removeSync('temp')
