694dee7e76d3293f1f244e0879aebf072815fddf
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-05-15 22:30:37 +0900

    :bug:  Change or function interface to accept function

--- src/or.ts
+++ src/or.ts
@@ -1,31 +1,41 @@
 // Copyright 2021-present the Fonction authors. All rights reserved. MIT license.
+import { advance } from './advance.ts'
 import { NN } from './NN.ts'
 import { FalsyLike } from './types/index.ts'
+import { AnyFn } from './types/index.ts'
 
 /**
  * Returns true if one or both of its arguments are true; otherwise false.
  *
  * @param a - The first input any value
  * @param b - The second input any value
- * @returns The result of `!!a || !!bb`
+ * @returns The result of `!!a || !!bb` (if argument is function, return value)
  *
+ * @remarks
+ * If you pass a function as an argument, return value will evaluate.
+
  * @example
  * ```ts
  * or(true, true) // true
  * or(false, true) // true
  * or(true, false) // true
  * or(false, false) // false
+ *
+ * or(() => 0, () => 1) // true
+ * or(() => 0, () => 0) // false
  * ```
+ *
+ * @category `Logic`
  *
  * @see Related to {@link and} {@link xor}
  *
  * @public
  */
 const or = <T, U>(
-  a: T,
-  b: U
+  a: T | AnyFn<any, T>,
+  b: U | AnyFn<any, U>
 ): T extends FalsyLike ? (U extends FalsyLike ? false : boolean) : boolean =>
-  (NN(a) || NN(b)) as T extends FalsyLike
+  (NN(advance(a)) || NN(advance(b))) as T extends FalsyLike
     ? U extends FalsyLike
       ? false
       : boolean
--- test/or.test.ts
+++ test/or.test.ts
@@ -1,7 +1,8 @@
 // Copyright 2021-present the Fonction authors. All rights reserved. MIT license.
-import { assertEquals } from '../dev_deps.ts'
+import { assertEquals, spy } from '../dev_deps.ts'
+import { length } from '../src/length.ts'
 import { or } from '../src/or.ts'
-import { Falsy } from './../src/types/index.ts'
+import { FalsyLike } from './../src/types/index.ts'
 import { assertEqual } from './asserts.ts'
 
 Deno.test('or', () => {
@@ -9,14 +10,38 @@ Deno.test('or', () => {
     [false, true, true],
     [true, false, true],
     [true, true, true],
-    [false, false, false]
+    [false, false, false],
+    [() => 1, () => 1, true],
+    [() => 1, () => 0, true],
+    [() => 0, () => 1, true],
+    [() => 0, () => 0, false],
+    [false, () => 0, false],
+    [false, () => 1, true]
   ]
   table.forEach(([a, b, expected]) => {
     assertEquals(or(a, b), expected, `or(${a}, ${b}) -> ${expected}`)
   })
 
-  assertEqual<false>(or(false as Falsy, false as Falsy))
+  const fn = spy(() => true)
+  const fn2 = spy(() => false)
+
+  or(fn, fn2)
+  assertEquals(length(fn2.calls), 0)
+
+  assertEqual<false>(or(false as FalsyLike, false as FalsyLike))
   assertEqual<boolean>(or(Boolean, Boolean))
+  assertEqual<boolean>(
+    or(
+      () => false as const,
+      () => true as const
+    )
+  )
+  assertEqual<false>(
+    or(
+      () => false as const,
+      () => false as const
+    )
+  )
 
   // Because Truthy can not define.
   assertEqual<boolean>(or(true as const, true as const))
