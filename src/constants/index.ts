10904477301fd104d9158ca3c2697f145991d07f
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-05-15 17:43:16 +0900

    :sparkles:  Add isJSONObject function

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,7 @@
 // Copyright 2021-present the Fonction authors. All rights reserved. MIT license.
 export { _ } from 'https://x.nest.land/arithmetic4@0.1.1/mod.ts'
 export const NULL = null
+export const JSON_OBJECT = 'Object'
 
 const { prototype, entries, keys, values } = Object
 const { hasOwnProperty } = prototype
---
+++ src/isJSONObject.ts
@@ -0,0 +1,27 @@
+// Copyright 2021-present the Fonction authors. All rights reserved. MIT license.
+
+import { constructorName } from './_/constructorName.ts'
+import { JSON_OBJECT } from './constants/index.ts'
+
+/**
+ * Whatever argument is JSON Object or not.
+ *
+ * @param val - Input any value
+ * @returns if `val` is JSON Object `true` otherwise; `false`
+ *
+ * @example
+ * ```ts
+ * isJSONObject({ hoge: 'huga'}) // true
+ * isJSONObject(Object()) // true
+ * isJSONObject(new Object()) // true
+ *
+ * isJSONObject([]) // false
+ * isJSONObject(new Set()) // false
+ * ```
+ *
+ * @beta
+ */
+const isJSONObject = (val: unknown): val is Record<PropertyKey, unknown> =>
+  constructorName(val) === JSON_OBJECT
+
+export { isJSONObject }
