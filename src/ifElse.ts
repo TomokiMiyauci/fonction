fefe6244605bc3adf2a20ac364a08c7ac3509449
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-05-15 13:39:24 +0900

    :bug:  Change ifElse function interface to accept any function

--- src/ifElse.ts
+++ src/ifElse.ts
@@ -1,5 +1,7 @@
 // Copyright 2021-present the Fonction authors. All rights reserved. MIT license.
+import { isFunction } from './isFunction.ts'
 import { NN } from './NN.ts'
+import { AnyFn } from './types/index.ts'
 import { FalsyLike } from './types/index.ts'
 /**
  * Return the `onTrue` or the `onFalse` value depending upon the result of the condition `val`.
@@ -14,6 +16,8 @@ import { FalsyLike } from './types/index.ts'
  * ifElse(true, 1, 0) // 1
  * ifElse(false, 1, 0) // 0
  * ifElse(undefined, 1, 0) // 0
+ * const result = () => true
+ * ifElse(result, () => 1, () => 0) // 1
  * ```
  *
  * @category `Logic`
@@ -23,14 +27,18 @@ import { FalsyLike } from './types/index.ts'
  * @public
  */
 const ifElse = <V, T, F>(
-  val: V,
-  onTrue: T,
-  onFalse: F
-): V extends FalsyLike ? F : V extends true ? T : T | F =>
-  (NN(val) ? onTrue : onFalse) as V extends FalsyLike
-    ? F
-    : V extends true
-    ? T
-    : T | F
+  val: V | AnyFn<any, V>,
+  onTrue: T | AnyFn<any, T>,
+  onFalse: F | AnyFn<any, F>
+): V extends FalsyLike ? F : V extends true ? T : T | F => {
+  const result = isFunction(val) ? NN(val()) : NN(val)
+  return result
+    ? isFunction(onTrue)
+      ? (onTrue() as V extends FalsyLike ? F : V extends true ? T : T | F)
+      : (onTrue as V extends FalsyLike ? F : V extends true ? T : T | F)
+    : isFunction(onFalse)
+    ? (onFalse() as V extends FalsyLike ? F : V extends true ? T : T | F)
+    : (onFalse as V extends FalsyLike ? F : V extends true ? T : T | F)
+}
 
 export { ifElse }
