9c833126ade4835f7d60e3440aa814bd5261af9c
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-06-03 14:50:26 +0900

    :sparkles:  Improve pipe function of type definition
    Closes #77

--- test/pipe.test.ts
+++ test/pipe.test.ts
@@ -8,14 +8,15 @@ import { AnyFn } from '../src/types/index.ts'
 import { assertEqual } from './asserts.ts'
 
 Deno.test('pipe', () => {
-  const table: [[AnyFn, AnyFn], unknown, unknown][] = [
+  const table: [AnyFn[], unknown, unknown][] = [
     [[() => {}, () => {}], '', undefined],
     [[() => {}, () => true], '', true],
     [[() => {}, () => true], 1, true],
     [[() => 1, (n: number) => n + 1], 1, 2],
     [[isNumber, N], '', true],
     [[N, N], 1, true],
-    [[isNumber, N], 1, false]
+    [[isNumber, N], 1, false],
+    [[() => true, () => false, () => true, () => false], 1, false]
   ]
 
   table.forEach(([[fn1, fn2], argument, expected]) => {
@@ -27,4 +28,27 @@ Deno.test('pipe', () => {
   })
 
   assertEqual<boolean>(pipe(isNumber, N)(1))
+
+  const pipetest = pipe(
+    (_: number, __: string, ___: number) => {
+      return 1 as const
+    },
+    (a) => a,
+    () => [] as [],
+    (a) => a,
+    (a) => a,
+    (a) => a,
+    (_) => '' as const,
+    (a) => a,
+    (a) => a,
+    (a) => a,
+    (a) => a,
+    (a) => a,
+    (_) => 1 as const,
+    (a) => a,
+    (a) => a,
+    (_) => '' as const,
+    (a) => a
+  )
+  assertEqual<''>(pipetest(1, '', 1))
 })
