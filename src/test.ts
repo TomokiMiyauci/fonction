3de8431ecdf6b8c9d1c0ddd15ce364a481235839
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-06-08 17:31:42 +0900

    :bulb:  Change docs code

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,8 @@
 // Copyright 2021-present the Fonction authors. All rights reserved. MIT license.
 import { curry2 } from './_/curry2.ts'
 
+const _test = (regExp: RegExp, val: string): boolean => regExp.test(val)
+
 /**
  * whether a given `string` matches a given regular expression.
  *
@@ -18,8 +20,6 @@ import { curry2 } from './_/curry2.ts'
  *
  * @beta
  */
-const _test = (regExp: RegExp, val: string): boolean => regExp.test(val)
-
 const test = curry2(_test)
 
 export { _test, test }
