76387ba7d274a1b487a7d2a5597a158cd8162dd1
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-06-28 18:03:15 +0900

    :bug:  Change heading logic

--- src/head.ts
+++ src/head.ts
@@ -1,37 +1,88 @@
 // Copyright 2021-present the Fonction authors. All rights reserved. MIT license.
-import { slice } from './slice.ts'
+import { ifElse } from './ifElse.ts'
+import { isArray } from './isArray.ts'
+import { length } from './length.ts'
+import { take } from './take.ts'
+import { String2Array } from './types/index.ts'
+
 /**
- * Returns all but the last element of the given list or string.
+ * Infer the head types.
  *
- * @param val - string or any array object
- * @returns The result of `val.slice(0, -1)`
+ * @typeParam T - `string` or any `array`
+ * @returns Head element of the `T`
  *
  * @example
  * ```ts
  * // String
- * head('hello') // 'hell'
- * head('h') // ''
- * head('') // ''
+ * Head<string> // string
+ * Head<''> // ''
+ * Head<'hello'> // 'h'
  * ```
  *
  * @example
  * ```ts
- * head([1, 2, 3]) // [1, 2]
- * head(['hello', 'world']) // ['hello']
- * head(['hello']) // []
- * head([]) // []
+ * // Array
+ * Head<[] | never[] | readonly [] | readonly never[]> // undefined
+ * Head<['hello', 'world']> // 'hello'
+ * Head<string | number[]> // string | number
  * ```
  *
  * @category `Array` `String`
  *
- * @see Related to {@link tail}
+ * @see Related to {@link Last}
  *
  * @public
  */
-const head: {
-  (val: string): string
-  <T extends unknown[]>(val: T): T
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-} = (val: any) => slice(0, -1, val)
+type Head<T extends readonly unknown[] | string> = T extends ''
+  ? ''
+  : T extends string
+  ? String2Array<T> extends []
+    ? string
+    : String2Array<T>[0]
+  : T extends readonly never[] | []
+  ? undefined
+  : T extends readonly [infer U, ...infer _]
+  ? U
+  : T[0] | undefined
 
+/**
+ * Infer the head types.
+ *
+ * @typeParam T - `string` or any `array`
+ * @returns Head element of the `T`
+ *
+ * @example
+ * ```ts
+ * // String
+ * Head<string> // string
+ * Head<''> // ''
+ * Head<'hello'> // 'h'
+ * ```
+ *
+ * @example
+ * ```ts
+ * // Array
+ * Head<[] | never[] | readonly [] | readonly never[]> // undefined
+ * Head<['hello', 'world']> // 'hello'
+ * Head<string | number[]> // string | number | undefined
+ * ```
+ *
+ * @category `Array` `String`
+ *
+ * @see Related to {@link Last}
+ *
+ * @public
+ */
+const head = <T extends readonly unknown[] | string>(val: T): Head<T> =>
+  ifElse(
+    isArray(val),
+    () =>
+      ifElse(
+        length(val as T & any[]),
+        () => take(1, val)[0] as Head<T>,
+        undefined as Head<T>
+      ),
+    () => take(1, val) as Head<T>
+  )
 export { head }
+export type { Head }
