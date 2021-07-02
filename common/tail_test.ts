f8458f579d07e20402a3c3f378b0b34b01ba375d
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-07-02 20:24:34 +0900

    :sparkles:  Add types of Tail

--- common/tail_test.ts
+++ common/tail_test.ts
@@ -1,6 +1,6 @@
 // Copyright 2021-present the Fonction authors. All rights reserved. MIT license.
-import { assertEquals } from '../dev_deps.ts'
-import { tail } from '../src/tail.ts'
+import { assertEquals, assertEqualsType } from '../dev_deps.ts'
+import { Tail, tail } from './tail.ts'
 
 Deno.test('tail', () => {
   const tableString: [string, string][] = [
@@ -37,4 +37,18 @@ Deno.test('tail', () => {
   tableArray.forEach(([val, expected]) => {
     assertEquals(tail(val), expected, `tail(${val}) -> ${expected}`)
   })
+
+  assertEqualsType<string, Tail<string>>()
+  assertEqualsType<'', Tail<''>>()
+  assertEqualsType<'', Tail<'a'>>()
+  assertEqualsType<'b', Tail<'ab'>>()
+  assertEqualsType<'bcdef', Tail<'abcdef'>>()
+  assertEqualsType<[], Tail<[]>>()
+  assertEqualsType<[], Tail<[1]>>()
+  assertEqualsType<[], Tail<readonly [1]>>()
+  assertEqualsType<[2], Tail<[1, 2]>>()
+  assertEqualsType<[2, 3, 4, 5], Tail<[1, 2, 3, 4, 5]>>()
+  assertEqualsType<[2, 3, 4, 5], Tail<readonly [1, 2, 3, 4, 5]>>()
+  assertEqualsType<string[], Tail<string[]>>()
+  assertEqualsType<readonly string[], Tail<readonly string[]>>()
 })
