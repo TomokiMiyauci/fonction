3e5b40103068ba440b3349b356e63d8f4809ec87
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-11-09 21:40:42 +0900

    :fire:  Remove unused

--- deps.ts
+++ deps.ts
@@ -1,5 +1,4 @@
 // Copyright 2021-present the Fonction authors. All rights reserved. MIT license.
-export { length } from 'https://deno.land/x/core_fn@v1.0.0-beta.14/mod.ts'
 export { slice } from 'https://deno.land/x/core_fn@v1.0.0-beta.14/uncurry/common/slice.ts'
 export { curry } from 'https://deno.land/x/curry@v1.0.0/mod.ts'
 export { equal } from 'https://deno.land/x/equal@v1.5.0/mod.ts'
